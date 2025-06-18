dc286bcd3a47d4d0a8116bed7376381b95ed9324
mfurqanabbas20 <you@example.com>
2025-06-18 21:08:03 +0500

    changing footer color

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,18 +2,18 @@ import twitter from '../assets/twitter.png'
 import facebook from '../assets/facebook.png'
 import instagram from '../assets/instagram.png'
 import linkedin from '../assets/linkedin.png'
-import JPW from '../assets/JPW.jpg'
+import JPW from '../assets/jpw_remove.png'
 import {Link} from 'react-router-dom'
 
 const Footer = () => {
   return (
-    <div className='flex mt-20 mb-6 poppins p-0 max-md:flex-none'>
+    <div className='flex mt-20 py-6 poppins p-0 max-md:flex-none bg-blue-900 text-white'>
         <div className="footer-logo-section w-1/3 flex flex-col justify-between px-5 max-md:px-0 max-md:hidden">
             <Link to='/' className="logo">
                 <img src={JPW} alt='Truck Image' className='h-20 w-28 object-cover'/>
             </Link>
             <div className="description">
-                <p className='text-sm'>JPW offers durable, high-performance pressure washers for homeowners and professionals, with a wide range of cleaning solutions.</p>
+                <p className='text-[13px] opacity-80'>JPW offers durable, high-performance pressure washers for homeowners and professionals, with a wide range of cleaning solutions.</p>
             </div>
             <div className="social-media-icons flex gap-2 items-center mt-2">
                 <img className='size-5' src={facebook} alt="" />
@@ -43,10 +43,12 @@ const Footer = () => {
         <div className="footer-section w-1/4 max-md:flex max-md:flex-col max-md:items-center text-sm max-sm:w-96">
             <h1 className='font-bold text-lg'>Contact</h1>
             <ul className='text-md opacity-70 flex flex-col gap-3 mt-4'>
-                <li className='cursor-pointer'>New Delhi, Pakistan</li>
-                <li className='cursor-pointer'>+92 300 1234567</li>
+                <li className='cursor-pointer'>112 Dorrington close 
+Bedfordshire,  Lu3 1xr 
+Luton,  United kingdom</li>
+                <li className='cursor-pointer'>07417480001</li>
                 <li className='cursor-pointer'>Take Record</li>
-                <li className='cursor-pointer'>www.facebook.com</li>
+                <li className='cursor-pointer'>jpw-store.vercel.com</li>
             </ul>
         </div>
         </div>
