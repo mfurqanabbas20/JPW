bfacfd94f1d3349d1411da5543fce860b1e01a42
mfurqanabbas20 <you@example.com>
2025-06-22 14:39:02 +0500

    Making social icons visible in footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -8,7 +8,8 @@ import {Link} from 'react-router-dom'
 const Footer = () => {
   return (
     <div className='flex mt-20 py-6 poppins p-0 max-md:flex-none bg-blue-900 text-white'>
-        <div className="footer-logo-section w-1/3 flex flex-col justify-between px-5 max-md:px-0 max-md:hidden">
+        <div className="footer-sections-container w-fullflex justify-around max-md:flex-wrap gap-5 max-md:w-screen max-md:text-center max-sm:justify-center">
+         <div className="footer-logo-section w-1/4 max-md:w-full flex flex-col justify-between max-md:items-center px-5 max-md:px-0">
             <Link to='/' className="logo">
                 <img src={JPW} alt='Truck Image' className='h-20 w-28 object-cover'/>
             </Link>
@@ -22,7 +23,6 @@ const Footer = () => {
                 <img className='size-5' src={linkedin} alt="" />
             </div>
         </div>
-        <div className="footer-sections-container w-full flex justify-around flex-wrap gap-5 max-md:w-screen max-md:text-center max-sm:justify-center">
         <div className="footer-section w-1/4 max-md:flex max-md:flex-col max-md:items-center max-sm:w-96">
             <h1 className='font-bold text-lg'>About Us</h1>
             <ul className='text-md opacity-70 flex flex-col gap-3 mt-4 text-sm'>
@@ -52,7 +52,6 @@ Luton,  United kingdom</li>
             </ul>
         </div>
         </div>
-
     </div>
   )
 }
