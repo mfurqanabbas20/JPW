9a45aa8fb8d28a1f1c4413b973af2ac7bfa28f13
Muhammad Furqan Abbas <muhammad.furqan@kryptomind.com>
2025-07-08 21:48:45 +0500

    adding api path in session url

--- frontend/src/components/UserInfo.jsx
+++ frontend/src/components/UserInfo.jsx
@@ -40,7 +40,7 @@ const UserInfo = ({product, productQuantity}) => {
           product: product,
           order: data
         }       
-        const session = await axios.post("https://jpw-flax.vercel.app/order/create-checkout-session", body)
+        const session = await axios.post("https://jpw-flax.vercel.app/api/order/create-checkout-session", body)
 
         const result = stripe.redirectToCheckout({
           sessionId: session.data.id
